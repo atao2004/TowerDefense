cc26a9cd67ec096a1078f6970788a84d9ac889d2
Daniel Lee <daniel.j.lee.2561@gmail.com>
2025-02-16 16:55:07 -0800

    Player Animation - Move

--- src/state_system.cpp
+++ src/state_system.cpp
@@ -2,16 +2,18 @@
 #include "state_system.hpp"
 #include <iostream>
 
+float StateSystem::timer_ms = 0;
+
 void StateSystem::step(float elapsed_ms)
 {
     Entity player = registry.players.entities[0];
-    RenderRequest& request = registry.renderRequests.get(player);
     State& state = registry.states.get(player);
 
+    timer_ms += elapsed_ms;
     if (state.state == STATE::IDLE) {
-        request.used_texture = TEXTURE_ASSET_ID::PLAYER_IDLE;
+        state_idle();
     } else {
-        request.used_texture = TEXTURE_ASSET_ID::PLAYER_WALK_1;
+        state_move();
     }
 }
 
@@ -23,6 +25,30 @@ void StateSystem::update_state(STATE state_new)
 
     if (state.state != state_new) {
         state.state = state_new;
-        // update timer
+        timer_ms = 0;
+    }
+}
+
+void StateSystem::state_idle()
+{
+    Entity player = registry.players.entities[0];
+    RenderRequest& request = registry.renderRequests.get(player);
+
+    request.used_texture = TEXTURE_ASSET_ID::PLAYER_IDLE;
+}
+
+void StateSystem::state_move()
+{
+    Entity player = registry.players.entities[0];
+    RenderRequest& request = registry.renderRequests.get(player);
+
+    if (timer_ms >= 1000) {
+        if (request.used_texture == TEXTURE_ASSET_ID::PLAYER_WALK_1) {
+            request.used_texture = TEXTURE_ASSET_ID::PLAYER_WALK_2;
+        }
+        else {
+            request.used_texture = TEXTURE_ASSET_ID::PLAYER_WALK_1;
+        }
+        timer_ms = 0;
     }
 }
--- src/state_system.hpp
+++ src/state_system.hpp
@@ -8,4 +8,7 @@ public:
     static void step(float elapsed_ms);
     static void update_state(STATE state_new);
 private:
+    static float timer_ms;
+    static void state_idle();
+    static void state_move();
 };
