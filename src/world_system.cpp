4f58b30952a090709ac026d67ee5d52db0f40538
kunglim <koolkung@gmail.com>
2025-02-20 16:01:16 -0800

    temp. disable of seed planting

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -459,10 +459,10 @@ void WorldSystem::on_key(int key, int, int action, int mod)
 	Motion& motion = registry.motions.get(player);
 
 	// Plant seed (for Milestone #2)
-	if (action == GLFW_PRESS && key == GLFW_KEY_F)
-	{
-		createSeed(vec2(motion.position.x, motion.position.y));
-	}
+	// if (action == GLFW_PRESS && key == GLFW_KEY_F)
+	// {
+	// 	createSeed(vec2(motion.position.x, motion.position.y));
+	// }
 	
 	// Move left
 	if (motion.position.x >= (PLAYER_WIDTH / 2) + SCORCHED_EARTH_BOUNDARY) {
